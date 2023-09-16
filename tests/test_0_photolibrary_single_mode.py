9068bc341e1b402f09bdf9513fbe34d16180738a
Rhet Turnbull <rturnbull@gmail.com>
2023-09-15 21:08:09 -0700

    Initial tests

--- tests/test_0_photolibrary_single_mode.py
+++ tests/test_0_photolibrary_single_mode.py
@@ -3,8 +3,15 @@
 import pytest
 
 import photokit
+from osxphotos.utils import get_system_library_path
 
 
 def test_photolibrary_multi_library_mode():
     """Test PhotoLibrary.multi_library_mode property."""
     assert not photokit.PhotoLibrary.multi_library_mode()
+
+
+def test_photolibrary_system_photo_library_path():
+    """Test PhotoLibrary.system_photo_library_path() method."""
+    library_path = get_system_library_path()
+    assert photokit.PhotoLibrary.system_photo_library_path() == library_path
--- tests/test_1_photolibrary_multi_mode.py
+++ tests/test_1_photolibrary_multi_mode.py
@@ -7,7 +7,8 @@ import photokit
 SYSTEM_LIBRARY_PATH = photokit.PhotoLibrary.system_photo_library_path()
 
 
-def test_photolibrary_multi_library_mode():
-    """Test PhotoLibrary.multi_library_mode property."""
-    pl = photokit.PhotoLibrary(SYSTEM_LIBRARY_PATH)
+def test_photolibrary_multi_library_mode_enable_multi_library_mode():
+    """Test PhotoLibrary.multi_library_mode property and enable_multi_library_mode() method."""
+    assert not photokit.PhotoLibrary.multi_library_mode()
+    photokit.PhotoLibrary.enable_multi_library_mode()
     assert photokit.PhotoLibrary.multi_library_mode()
