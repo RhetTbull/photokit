95c69122ca0099cb7a776e313dd56f4334e9a5ee
Rhet Turnbull <rturnbull@gmail.com>
2023-09-17 08:08:15 -0700

    Version bump

--- photokit/__init__.py
+++ photokit/__init__.py
@@ -1,6 +1,6 @@
 """Python package for accessing the macOS Photos.app library via Apple's native PhotoKit framework."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 import logging
 
 from .asset import (
